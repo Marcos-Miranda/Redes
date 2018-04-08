1c1e7817cc6ff7cd29ac070a5b16072098f1747c
Markinnmn <30961631+Markinnmn@users.noreply.github.com>
2018-04-07 23:10:27 -0300

    Update server.py
    Final

--- server.py
+++ server.py
@@ -1,28 +1,33 @@
-# importação do módulo socket
+# Importação do módulo socket
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepara o socket do servidor
-serverSocket.bind(('', 80))
+serverSocket.bind(('', 78))
 serverSocket.listen(1)
 while True:
-    print('O servidor está pronto para receber requisições...')
-    # Estabelece a conexão com o cliente
-    connectionSocket, addr = serverSocket.accept()
-    try:
-        mensagem = connectionSocket.recv(1024)
-        nome_arquivo = mensagem.split()[1]
-        arquivo = open(nome_arquivo[1:])
-        saida = arquivo.read()
-        arquivo.close()
-        # Envia linhas de cabeçalho
-        connectionSocket.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
-        # Envia o conteúdo do arquivo requisitado ao cliente
-        connectionSocket.send(saida.encode())
-        connectionSocket.close()
-    except IOError:
-        # Envia resposta de arquivo não encontrado
-        connectionSocket.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
-        connectionSocket.send(('<h1>404 Not Found</h1>').encode())
-        # Encerra a conexão com o cliente
-        connectionSocket.close()
+	print('O servidor está pronto para receber requisições...')
+	# Estabelece a conexão com o cliente
+	connectionSocket, addr = serverSocket.accept()
+	print('Conexão estabelecida')
+	try:
+		mensagem = connectionSocket.recv(1024).decode();
+		print('Mensagem recebida')
+		if not mensagem:
+			nome_arquivo = 'erro404'
+		else:
+			nome_arquivo = mensagem.split()[1]
+		arquivo = open(nome_arquivo[1:])
+		saida = arquivo.read()
+		arquivo.close()
+		# Envia linhas de cabeçalho
+		connectionSocket.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
+		# Envia o conteúdo do arquivo requisitado ao cliente
+		connectionSocket.send(saida.encode())
+		connectionSocket.close()
+	except IOError:
+		# Envia resposta de arquivo não encontrado
+		connectionSocket.send(('HTTP/1.1 404 Not Found\r\n\r\n').encode())
+		connectionSocket.send(('<h1>404 Not Found</h1>').encode())
+		# Encerra a conexão com o cliente
+		connectionSocket.close()
 serverSocket.close()
